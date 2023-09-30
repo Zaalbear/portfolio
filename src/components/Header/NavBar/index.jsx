5855da45ef2d3a02a07d4bcb9cc866902599d878
Zaalbear2 <zaalbear@gmail.com>
2023-09-30 01:56:50 -0300

    feat: criada base do projeto react.js com vite

---
+++ src/components/Header/NavBar/index.jsx
@@ -0,0 +1,12 @@
+export const NavBar = () => {
+  return (
+    <ul>
+        <li>home</li>
+        <li>about</li>
+        <li>projects</li>
+        <li>services</li>
+        <li>resume</li>
+        <li>contact</li>
+    </ul>
+  )
+}
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import { NavBar } from "./NavBar"
+
+export const Header = () => {
+  return (
+    <header>
+      <div>
+        <h1>João Victor "Zaalbear" Barcellos</h1>
+        <p>web developer</p>
+      </div>
+        <nav>
+          <NavBar/>
+        </nav>
+    </header>
+  )
+}
