656e3d7188f3171f0eb2c75c5fa47d9a99334aa9
zaalbear <zaalbear@ZaalbearNote.zaalbear>
2024-04-26 17:24:22 -0300

    added projects images

--- src/data/projects.database.js
+++ src/data/projects.database.js
@@ -1,108 +1,35 @@
 import hub_img from "../assets/prints/kenzie_hub.png"
-import bank_img from "../assets/prints/projeto_bank.png"
 import hamb_img from "../assets/prints/projeto_hamburgueria.png"
 import port_img from "../assets/prints/projeto_portfolio.png"
+import bank_img from "../assets/prints/projeto_bank.png"
 
 
 export const projectsList = [
     {
-        name: 'Portfolio',
+        name: 'Kenzie hub',
         repoURL: 'https://github.com/Zaalbear/react-entrega-kenzie-hub_zaalbear',
-        deployURL: 'https://react-entrega-kenzie-hub-zaalbear.vercel.app/',
-        imgURL: port_img,
-        techs: [{
-            name: 'HTML',
-            icon: "devicon-html5-plain-wordmark",
-        },
-        {
-            name: 'CSS',
-            icon: "devicon-css3-plain-wordmark",
-        },
-        {
-            name: 'Sass',
-            icon: 'devicon-sass-original',
-        }],
+        imgURL: hub_img,
         field: 'front',
-        isDeployed: true,
         isNew: true,
     },
     {
+        name: 'Portfolio',
+        repoURL: 'https://github.com/Zaalbear/m3-s1-entrega-portfolio_zaalbear',
+        imgURL: port_img,
+        field: 'front',
+        isNew: false,
+    },
+    {
         name: 'NuKenzie',
-        repoURL: 'https://github.com/Zaalbear/react-entrega-kenzie-hub_zaalbear',
-        deployURL: 'https://react-entrega-kenzie-hub-zaalbear.vercel.app/',
-        imgURL: hamb_img,
-        techs: [{
-            name: 'HTML',
-            icon: "devicon-html5-plain-wordmark",
-        },
-        {
-            name: 'CSS',
-            icon: "devicon-css3-plain-wordmark",
-        },
-        {
-            name: 'Sass',
-            icon: 'devicon-sass-original',
-        }],
-        field: 'back',
+        repoURL: 'https://github.com/Zaalbear/react-entrega-s1-nu-kenzie_zaalbear',
+        imgURL: bank_img,
+        field: 'front',
         isNew: false,
     },
     {
         name: 'Hamburgueria',
-        repoURL: 'https://github.com/Zaalbear/react-entrega-kenzie-hub_zaalbear',
-        deployURL: 'https://react-entrega-kenzie-hub-zaalbear.vercel.app/',
+        repoURL: 'https://github.com/Zaalbear/react-entrega-s3-hamgurgueria_zaalbear',
         imgURL: hamb_img,
-        techs: [{
-            name: 'HTML',
-            icon: "devicon-html5-plain-wordmark",
-        },
-        {
-            name: 'CSS',
-            icon: "devicon-css3-plain-wordmark",
-        },
-        {
-            name: 'Sass',
-            icon: 'devicon-sass-original',
-        }],
-        field: 'front',
-        isNew: false,
-    },
-    {
-        name: 'Kenzie hub',
-        repoURL: 'https://github.com/Zaalbear/react-entrega-kenzie-hub_zaalbear',
-        deployURL: 'https://react-entrega-kenzie-hub-zaalbear.vercel.app/',
-        imgURL: hub_img,
-        techs: [{
-            name: 'HTML',
-            icon: "devicon-html5-plain-wordmark",
-        },
-        {
-            name: 'CSS',
-            icon: "devicon-css3-plain-wordmark",
-        },
-        {
-            name: 'Sass',
-            icon: 'devicon-sass-original',
-        }],
-        field: 'back',
-        isNew: false,
-    },
-    {
-        name: 'kenzie hub',
-        repoURL: 'https://github.com/Zaalbear/react-entrega-kenzie-hub_zaalbear',
-        deployURL: 'https://react-entrega-kenzie-hub-zaalbear.vercel.app/',
-        imgURL: hub_img,
-        techs: [{
-            name: 'HTML',
-            icon: "devicon-html5-plain-wordmark",
-        },
-        {
-            name: 'CSS',
-            icon: "devicon-css3-plain-wordmark",
-        },
-        {
-            name: 'Sass',
-            icon: 'devicon-sass-original',
-        }],
         field: 'front',
         isNew: false,
     },
